48cd58a67792867eb4302bdba6c35ef803184779
LizaFridman <Liza.fridman39@gmail.com>
2019-04-19 15:53:06 +0300

    Rock movement
    (22) Coroutines & wait times in Unity 3D

---
+++ Haunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    private readonly float rockSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Move(bottomPosition));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Move(Vector3 targetPosition) {
+        while (Mathf.Abs((targetPosition - transform.localPosition).y) > 0.2f) {
+            Vector3 direction = (targetPosition.y == topPosition.y) ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * rockSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        print("Reached the target");
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = targetPosition.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(Move(newTarget));
+    }
+}
