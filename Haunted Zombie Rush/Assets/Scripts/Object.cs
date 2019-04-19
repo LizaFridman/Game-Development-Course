5a15a6de5d23049daad0e06e9ad0c03f097d293c
LizaFridman <Liza.fridman39@gmail.com>
2019-04-19 15:07:21 +0300

    Created movement of endless platforms
    (21) Moving objects in Unity 3D

---
+++ Haunted Zombie Rush/Assets/Scripts/Object.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    [SerializeField] private float objectSpeed = 1f;
+     private float resetPosition = -24.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+        if (transform.localPosition.x <= resetPosition) {
+            Vector3 newPosition = new Vector3(79.95f, transform.localPosition.y, transform.localPosition.z);
+            transform.position = newPosition;
+        }
+    }
+}
