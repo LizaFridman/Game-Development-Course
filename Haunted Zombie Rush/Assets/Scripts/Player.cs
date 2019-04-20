7237194b9210be9ec793f738e0e2cd7a6c38090d
LizaFridman <Liza.fridman39@gmail.com>
2019-04-20 11:32:19 +0300

    Added sound effects
    (26) Working with audio in Unity 3D

--- Haunted Zombie Rush/Assets/Scripts/Player.cs
+++ Haunted Zombie Rush/Assets/Scripts/Player.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private AudioClip sfxJump;
     private Animator animator;
     private Rigidbody rigidBody;
     private const int LeftClick = 0;
-
     private bool isJump = false;
+    private AudioSource audioSource;
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
     
     void Update()
     {
         if (Input.GetMouseButtonDown(LeftClick)) {
             animator.Play("Jump");
+            audioSource.PlayOneShot(sfxJump);
             rigidBody.useGravity = true;
             isJump = true;
         }
