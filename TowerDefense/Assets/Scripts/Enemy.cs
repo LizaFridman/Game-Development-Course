e7522e58619a5a23bceb6bd97b9c3c345dee226e
LizaFridman <Liza.fridman39@gmail.com>
2019-05-25 15:51:41 +0300

    Added enemy pathfinding

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int target = 0;
+    public Transform exitPoint;
+    public Transform[] waypoints;
+    public float navigationUpdate;
+
+    private Transform enemyLocation;
+    private float navigationTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyLocation = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypoints != null) {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate) {
+                if (target < waypoints.Length)
+                {
+                    enemyLocation.position = Vector2.MoveTowards(enemyLocation.position, waypoints[target].position, navigationTime);
+                }
+                else {
+                    enemyLocation.position = Vector2.MoveTowards(enemyLocation.position, exitPoint.position, navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Checkpoint")
+        {
+            target++;
+        }
+        else if(other.tag == "Finish"){
+            GameManager.instance.RemoveEnemyFromScreen();
+            Destroy(gameObject);
+        }
+    }
+}
--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     public int enemiesPerSpawn;
 
     private int enemiesOnScreen = 0;
+    private const float spawnDelay = 0.5f;
 
     void Awake()
     {
@@ -30,18 +31,30 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemy();
+        StartCoroutine(Spawn());
     }
-
-    private void SpawnEnemy() {
-        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
-            for (var i = 0; i < enemiesPerSpawn; i++) {
-                if (enemiesOnScreen < maxEnemiesOnScreen) {
-                    var newEnemy = Instantiate(enemies[0]) as GameObject;
+    
+    IEnumerator Spawn() {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+        {
+            for (var i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemiesOnScreen)
+                {
+                    int rand = (int)Random.Range(0,3);
+                    var newEnemy = Instantiate(enemies[rand]) as GameObject;
                     newEnemy.transform.position = spawnPoint.transform.position;
                     enemiesOnScreen++;
                 }
             }
+            yield return new WaitForSeconds(spawnDelay);
+            StartCoroutine(Spawn());
+        }
+    }
+
+    public void RemoveEnemyFromScreen() {
+        if (enemiesOnScreen > 0) {
+            enemiesOnScreen--;
         }
     }
 }
