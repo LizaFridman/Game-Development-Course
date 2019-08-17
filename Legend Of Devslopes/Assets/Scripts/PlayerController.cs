c9eca8b114ee6ef4d6dac5d245ffc14371552981
LizaFridman <Liza.fridman39@gmail.com>
2019-08-17 21:09:11 +0300

    Added ray cast for character rotation

--- Legend Of Devslopes/Assets/Scripts/PlayerController.cs
+++ Legend Of Devslopes/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10.0f;
+    [SerializeField] private LayerMask layerMask;
+
     private CharacterController characterController;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +21,24 @@ public class PlayerController : MonoBehaviour
     {
         var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
+
+        if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore)) {
+            if (hit.point != currentLookTarget) {
+                currentLookTarget = hit.point;
+            }
+
+            // character rotation
+            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+
+        }
     }
 }
