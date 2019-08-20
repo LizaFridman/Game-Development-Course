5877729217396ff8c9a6f50e432b324e92a85812
LizaFridman <Liza.fridman39@gmail.com>
2019-08-20 16:39:53 +0300

    Enemy attack animation in range

---
+++ Legend Of Devslopes/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private float range = 3f;
+    [SerializeField] private float timeBetweenAttacks = 1f;
+
+    private Animator anim;
+    private GameObject player;
+    private bool isPlayerInRange;
+    private BoxCollider[] weaponColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponColliders = GetComponentsInChildren<BoxCollider>();
+        player = GameManager.instance.Player;
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(Attack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isPlayerInRange = (Vector3.Distance(transform.position, player.transform.position) < range);
+        //print("Player in range = " + isPlayerInRange);
+    }
+
+    IEnumerator Attack() {
+        if (isPlayerInRange && !GameManager.instance.GameOver) {
+            anim.Play("Attack");
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+
+        yield return null;
+        StartCoroutine(Attack());
+    }
+}
