9cc80c3c6ce63d412a2bf4938a566adc229133dc
LizaFridman <Liza.fridman39@gmail.com>
2019-08-20 17:17:22 +0300

    Animation events to enemies (Except Ranger)

--- Legend Of Devslopes/Assets/Scripts/PlayerController.cs
+++ Legend Of Devslopes/Assets/Scripts/PlayerController.cs
@@ -21,42 +21,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.SimpleMove(moveDirection * moveSpeed);
-
-        if (moveDirection == Vector3.zero)
+        if (!GameManager.instance.GameOver)
         {
-            anim.SetBool("IsWalking", false);
-        }
-        else {
-            anim.SetBool("IsWalking", true);
-        }
+            var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            characterController.SimpleMove(moveDirection * moveSpeed);
 
-        if (Input.GetMouseButtonDown(0)) { // Left Button
-            anim.Play("DoubleChop");
-        }
+            if (moveDirection == Vector3.zero)
+            {
+                anim.SetBool("IsWalking", false);
+            }
+            else
+            {
+                anim.SetBool("IsWalking", true);
+            }
 
-        if (Input.GetMouseButtonDown(1)) {// Right Button
-            anim.Play("SpinAttack");
+            if (Input.GetMouseButtonDown(0))
+            { // Left Button
+                anim.Play("DoubleChop");
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {// Right Button
+                anim.Play("SpinAttack");
+            }
         }
     }
 
     void FixedUpdate() {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!GameManager.instance.GameOver)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
+            Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
 
-        if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore)) {
-            if (hit.point != currentLookTarget) {
-                currentLookTarget = hit.point;
+            if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore))
+            {
+                if (hit.point != currentLookTarget)
+                {
+                    currentLookTarget = hit.point;
+                }
+
+                // character rotation
+                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+
             }
-
-            // character rotation
-            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
-
         }
     }
 }
---
+++ Legend Of Devslopes/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 100;
+    [SerializeField] float timeSinceLastHitInSeconds = 2f;
+
+    private float timer = 0f;
+    private CharacterController characterController;
+    private Animator anim;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (timer >= timeSinceLastHitInSeconds &&
+            !GameManager.instance.GameOver) {
+
+            if (other.tag == "Weapon") {
+                TakeHit();
+                timer = 0;
+            }
+        }
+    }
+
+    void TakeHit() {
+        if (currentHealth > 0) {
+            GameManager.instance.PlayerHit(currentHealth);
+            anim.Play("Hurt");
+            currentHealth -= 10;
+        }
+
+        if (currentHealth <= 0) {
+            KillPlayer();
+        }
+    }
+
+    void KillPlayer() {
+        GameManager.instance.PlayerHit(currentHealth);
+        anim.SetTrigger("HeroDie");
+        characterController.enabled = false;
+    }
+}
