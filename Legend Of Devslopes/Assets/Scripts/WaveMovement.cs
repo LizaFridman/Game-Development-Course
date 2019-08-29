e7b17ed3ef530322bc2a544a4e33d6ae5c2b3c6b
LizaFridman <Liza.fridman39@gmail.com>
2019-08-29 22:13:33 +0300

    Added wave movement to the water

--- Legend Of Devslopes/Assets/Scripts/WaveMovement.cs
+++ Legend Of Devslopes/Assets/Scripts/WaveMovement.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class WaveMovement : MonoBehaviour
 {
+    [SerializeField] private float waveHeight;
+    [SerializeField] private float tideSpeed;
+
+    private Vector3 orgPosition;
+    private Vector3 minPosition;
+    private Vector3 maxPosition;
+
+    private bool isMovingUp = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        orgPosition = transform.position;
+        minPosition = orgPosition + (Vector3.down * waveHeight);
+        maxPosition = orgPosition + (Vector3.up * waveHeight);
+
+        StartCoroutine(MoveUp());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    IEnumerator MoveUp()
+    {
+        Debug.Log("Moving Up");
+
+        while (IsFirstAboveSecond(maxPosition, transform.position, Vector3.up))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, maxPosition, Time.deltaTime * tideSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Finished Moving Up");
+        StartCoroutine(MoveDown());
+    }
+
+    IEnumerator MoveDown()
+    {
+        Debug.Log("Moving Down");
+
+        while (IsFirstAboveSecond(transform.position, minPosition, Vector3.up))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, minPosition, Time.deltaTime * tideSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Finished Moving Down");
+        StartCoroutine(MoveUp());
+    }
+
+    public static bool IsFirstAboveSecond(Vector3 a, Vector3 b, Vector3 up)
+    {
+        //return Vector3.Dot(b - a, up) < 0;
+        return (up.y > 0) ? (a.y > b.y) : (a.y < b.y);
     }
 }
