64d68aac4fb4dabd2bb1a83850bd0821adcdda11
LizaFridman <Liza.fridman39@gmail.com>
2019-10-01 20:37:03 +0300

    Added Menu scene

---
+++ Legend Of Devslopes/Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public void BeginAttack() { }
+    public void EndAttack() { }
+
+    public void EnemyBeginAttack() { }
+    public void EnemyEndAttack() { }
+
+    public void FireArrow() { }
+}
