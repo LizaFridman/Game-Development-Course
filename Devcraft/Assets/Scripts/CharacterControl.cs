6af8d7bb5a2f595cc14bbf2bb20eae793be57a36
LizaFridman <Liza.fridman39@gmail.com>
2019-10-14 16:37:43 +0300

    Control character 0.5
    1. Fixed animation controll
    2. Added GameManager
    3. Added Skybox
    4. Character Controller

---
+++ Devcraft/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] int movementSpeed;
+    [SerializeField] int jumpHeight;
+
+    private Rigidbody rigidBody;
+    private Animator animator;
+
+    //[SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        Assert.IsNotNull(rigidBody);
+        animator = GetComponent<Animator>();
+        Assert.IsNotNull(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveCharecter = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
+        transform.position += moveCharecter * Time.deltaTime * movementSpeed;
+
+        if (rigidBody.velocity.magnitude == 0)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+        }
+
+        if (GameManager.Instance.IsJumping)
+        {
+            animator.SetTrigger("Jump");
+            rigidBody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            GameManager.Instance.IsJumping = false;
+        }
+
+        if (GameManager.Instance.IsPunching)
+        {
+            animator.SetTrigger("Punch");
+            GameManager.Instance.IsPunching = false;
+        }
+
+        if (GameManager.Instance.IsBuilding)
+        {
+            animator.SetTrigger("Punch");
+            GameManager.Instance.IsBuilding = false;
+        }
+    }
+}
---
+++ Devcraft/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private bool isJumping = false;
+    private bool isPunching = false;
+    private bool isBuilding = false;
+
+    public bool IsJumping { get => isJumping; set => isJumping = value; }
+    public bool IsPunching { get => isPunching; set => isPunching = value; }
+    public bool IsBuilding { get => isBuilding; set => isBuilding = value; }
+
+    public void JumpButtonPressed()
+    {
+        IsJumping = true;
+    }
+
+    public void PunchButtonPressed()
+    {
+        IsPunching = true;
+    }
+
+    public void BuildButtonPressed()
+    {
+        IsBuilding = true;
+    }
+}
